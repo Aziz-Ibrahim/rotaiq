daf8227794cd71cfeee813bf535a387c95505b3c
Aziz-Ibrahim <dsnxc622@students.codeinstitute.net>
2025-08-22 13:45:01 +0100

    build(backend); Created a serializer to convert django models into json

--- backend/rota_gaps_app/settings.py
+++ backend/rota_gaps_app/settings.py
@@ -42,7 +42,7 @@ INSTALLED_APPS = [
     'rest_framework',
 
     # Custom Apps
-    'shifts',
+    'shifts.apps.ShiftsConfig',
 ]
 
 # Custom user model
---
+++ backend/shifts/serializer.py
@@ -0,0 +1,73 @@
+from rest_framework import serializers
+
+from .models import User, Branch, Shift
+
+
+class BranchSerializer(serializers.ModelSerializer):
+    """
+    Serializes Branch model instances into a JSON format.
+
+    This serializer is used to represent branch data in the API. It includes
+    all fields from the Branch model.
+    """
+    class Meta:
+        """
+        Meta options for the BranchSerializer.
+        """
+        model = Branch
+        fields = '__all__'
+
+
+class UserSerializer(serializers.ModelSerializer):
+    """
+    Serializes User model instances for read-only purposes.
+
+    This serializer is used to display user information without exposing
+    sensitive data like passwords. It includes a nested representation of
+    the user's branch.
+
+    Attributes:
+        branch (BranchSerializer):
+        A nested serializer to represent the user's branch details.
+    """
+    branch = BranchSerializer(read_only=True)
+
+    class Meta:
+        """
+        Meta options for the UserSerializer.
+        """
+        model = User
+        fields = ['id', 'email', 'first_name', 'last_name', 'role', 'branch']
+
+
+class ShiftSerializer(serializers.ModelSerializer):
+    """
+    Serializes Shift model instances for API operations.
+
+    This serializer handles the creation, viewing, and claiming of shifts.
+    It provides nested details for the branch, and users who posted and
+    claimed the shift.
+
+    Attributes:
+        branch_details (BranchSerializer):
+        A nested serializer to provide read-only details about the branch.
+        posted_by_details (UserSerializer):
+        A nested serializer for the user who posted the shift.
+        claimed_by_details (UserSerializer):
+        A nested serializer for the user who claimed the shift.
+    """
+    branch_details = BranchSerializer(source='branch', read_only=True)
+    posted_by_details = UserSerializer(source='posted_by', read_only=True)
+    claimed_by_details = UserSerializer(source='claimed_by', read_only=True)
+
+    class Meta:
+        """
+        Meta options for the ShiftSerializer.
+        """
+        model = Shift
+        fields = [
+            'id', 'branch', 'branch_details', 'posted_by', 'posted_by_details',
+            'claimed_by', 'claimed_by_details', 'start_time', 'end_time',
+            'role', 'status', 'description'
+        ]
+        read_only_fields = ['status', 'posted_by', 'claimed_by']
