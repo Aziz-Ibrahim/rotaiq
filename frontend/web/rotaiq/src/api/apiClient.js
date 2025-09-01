e3da36808b9eb69faed5fbcc9f11eca6692048be
Aziz-Ibrahim <dsnxc622@students.codeinstitute.net>
2025-09-01 13:39:35 +0100

    fix(BaseURL); hardcoded baseurl to match backend web service

--- frontend/web/rotaiq/src/api/apiClient.js
+++ frontend/web/rotaiq/src/api/apiClient.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import { jwtDecode } from 'jwt-decode';
 
-const API_URL = process.env.REACT_APP_API_URL || 'http://localhost:8000/api/';
+const API_URL = 'https://rotaiq-backend.onrender.com/';
 
 const apiClient = axios.create({
     baseURL: API_URL,
